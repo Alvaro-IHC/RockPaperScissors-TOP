2c8ccee0f6ec735be9f4c426316d6d27df5b4d86
Alvaro-IHC <alva0741@gmail.com>
2021-10-16 21:01:43 -0400

    Rock paper scissors completed

--- js/scriptJS.js
+++ js/scriptJS.js
@@ -1,3 +1,5 @@
+let numberUserWins = 0;
+let numberComputerWins = 0; 
 let capitalize = word => {
     word = word.toLowerCase();
     word = word.substr(0, 1).toUpperCase() + word.substr(1)
@@ -15,36 +17,39 @@ function playRound(playerSelection, computerSelection){
     let playerValue = arr.indexOf(capitalize(playerSelection));
     let computerValue = arr.indexOf(capitalize(computerSelection));
     if(playerValue === (computerValue+1)%3){
+        numberUserWins++;
         return 3; // win
     }else if ((playerValue+1)%3 === computerValue){
+        numberComputerWins++;
         return 2; // lose
     }else{
         return 1; // draw
     }
 }
 
-function game(){
-    let numberWin = 0;
-    let numberLose = 0;
-    for(let i = 0; i < 5; i++){
-        let playerSelection = prompt("Rock, paper or scissors?")
-        let computerSelection = computerPlay();
-        alert(`Computer has selected ${computerSelection}`);
-        let result = playRound(playerSelection, computerSelection);
-        if (result === 3){
-            alert(`You win this round! ${capitalize(playerSelection)} beats ${computerSelection}`);
-            numberWin++;
-        }else if(result === 2){
-            alert(`You lose this round! ${computerSelection} beats ${capitalize(playerSelection)}`)
-            numberLose++;
-        }else{
-            alert('DRAW');
-        }
+function game(playerSelection){
+    let computerSelection = computerPlay();
+    const message = `Computer has selected ${computerSelection}`+'<br>';
+    let result = playRound(playerSelection, computerSelection);
+    const elmResult = document.querySelector('.result');
+    const elmScore = document.querySelector('.score');
+    elmScore.textContent = `Score: You=${numberUserWins}, Computer=${numberComputerWins}`;
+    if (result === 3){
+        elmResult.innerHTML = message + `You win this round! ${capitalize(playerSelection)} beats ${computerSelection}`;       
+    }else if(result === 2){
+        elmResult.innerHTML = message + `You lose this round! ${computerSelection} beats ${capitalize(playerSelection)}`
+    }else{
+        elmResult.innerHTML = message + 'DRAW';
     }
-    if (numberWin > numberLose)
-        alert(`After 5 rounds, YOU WIN!`);
-    else if (numberLose > numberWin)
-        alert(`After 5 rounds, COMPUTER WINS!`);
-    else
-        alert('After 5 rounds, It\'s DRAW')
+
+    if (numberUserWins === 5){
+        elmScore.textContent='You got 5 points, you win';
+        numberComputerWins = 0;
+        numberUserWins = 0;
+    }else if(numberComputerWins === 5){
+        elmScore.textContent='Computer got 5 points, computer wins';
+        numberComputerWins = 0;
+        numberUserWins = 0;
+    }
+
 }
